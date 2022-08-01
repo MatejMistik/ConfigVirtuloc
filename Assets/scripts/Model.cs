8532ff91378ddf21e8d4ff07b8b77f0610f04ec7
MatejMistik <xmisti00@vutbr.cz>
2022-08-01 12:04:14 +0200

    namespace fix

--- Assets/scripts/Model.cs
+++ Assets/scripts/Model.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-public class Downloader : MonoBehaviour
+public class Model : MonoBehaviour
 {
 
     [SerializeField] Text textHtml;
